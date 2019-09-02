3d381d2a74cfd5aea9b2d21e8f80f6cb042072e5
Ctzzhang <tzzhang_021@163.com>
2019-09-02 21:29:49 +0800

    first commit
    Signed-off-by: Ctzzhang <tzzhang_021@163.com>

---
+++ src/main/java/com/everything/movie/repository/IMovieRepository.java
@@ -0,0 +1,17 @@
+package com.everything.movie.repository;
+
+
+import com.everything.movie.entity.Movie;
+import com.everything.movie.entity.Page;
+import com.everything.movie.entity.QueryDTO;
+
+public interface IMovieRepository {
+
+    boolean save(Movie movie);
+
+    Page<Movie> query(String queryString, int pageNo, int size);
+
+    Page<Movie> query(QueryDTO queryDTO, int pageNo, int size);
+
+    Movie get(String id);
+}
---
+++ src/main/java/com/everything/movie/web/SearchController.java
@@ -0,0 +1,63 @@
+package com.everything.movie.web;
+
+
+import com.everything.movie.entity.Movie;
+import com.everything.movie.entity.Page;
+import com.everything.movie.entity.QueryDTO;
+import com.everything.movie.repository.IMovieRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.stream.Collectors.*;
+
+@Controller
+@Slf4j
+public class SearchController {
+
+    @Autowired
+    private IMovieRepository movieRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        QueryDTO queryDTO = QueryDTO.builder().minScore(7.5f).orderBy("updateDate").build();
+        Page<Movie> page = movieRepository.query(queryDTO, 1, 6);
+        List<String> recommendWord = Collections.emptyList();
+        if (page != null) {
+            recommendWord = page.getList().stream().map(Movie::getRecommendWord).collect(toList());
+        }
+        model.addAttribute("recommendWord", recommendWord);
+        return "index";
+    }
+
+    @RequestMapping("/s")
+    public String search(
+            @RequestParam("wd") String queryString,
+            @RequestParam(value = "pn", required = false, defaultValue = "1") int pageNo,
+            Model model
+    ) {
+        log.info("搜索参数wd:{},pn:{}", queryString, pageNo);
+        Page<Movie> page = movieRepository.query(queryString, pageNo, 10);
+        model.addAttribute("page", page);
+        model.addAttribute("wd", queryString);
+        return "search";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(
+            @PathVariable("id") String id,
+            Model model
+    ) {
+        Movie movie = movieRepository.get(id);
+        model.addAttribute("movie", movie);
+        return "detail";
+    }
+}
